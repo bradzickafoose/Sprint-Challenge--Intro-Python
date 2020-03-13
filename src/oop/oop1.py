ff392ea212134e873af9c53d5a314e46bf70bb0c
Brad <brad@viziworx.com>
2020-03-13 11:19:20 -0400

    adds inheritance classes for Flight Vehicles and Ground Vehicles

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -27,5 +27,21 @@ class FlightVehicle(Vehicle):
     pass
 
 
+class Airplane(FlightVehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
 class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
     pass
