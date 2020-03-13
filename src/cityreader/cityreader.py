de91d9a6ab86b4b64cbe32c3138d253aeab6408e
Brad <brad@viziworx.com>
2020-03-13 13:31:16 -0400

    adds functionality to read from the 'cities.csv' file by importing the reader from csv module and creating a new City instance and adding it to the `cities` list. Removes unnecessary latitude and longitude descriptions from City class return statement to match requested print instructions of (name, lat, lon). Passes test.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,3 +1,4 @@
+from csv import reader
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
@@ -9,7 +10,7 @@ class City:
         self.lon = lon
 
     def __str__(self):
-        return f"{self.name}: latitude {self.lat}, longitude {self.lon}"
+        return f"{self.name}, {self.lat}, {self.lon}"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -31,7 +32,13 @@ def cityreader(cities=[]):
     # TODO Implement the functionality to read from the 'cities.csv' file
     # For each city record, create a new City instance and add it to the
     # `cities` list
-
+    with open("cities.csv", mode="r") as csv_file:
+        file = reader(csv_file)
+        for row in file:
+            if row[0] == "city":
+                continue
+            else:
+                cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
 
 
